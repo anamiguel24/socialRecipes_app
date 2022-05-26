e5aa4fd559ceef786bfacb92225f4e9f942747f8
florentino <florentino@ua.pt>
2022-05-26 01:16:46 +0100

    userProfile link + bottom bar fix

--- repSocialApp/app/src/main/java/com/example/recipessocialapp/feed/FeedActivity.java
+++ repSocialApp/app/src/main/java/com/example/recipessocialapp/feed/FeedActivity.java
@@ -1,11 +1,16 @@
 package com.example.recipessocialapp.feed;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.example.recipessocialapp.R;
 import com.example.recipessocialapp.databinding.ActivityFeedBinding;
+import com.example.recipessocialapp.profilePages.UserProfileActivity;
 
 public class FeedActivity extends AppCompatActivity {
     private ActivityFeedBinding binding;
@@ -16,6 +21,38 @@ public class FeedActivity extends AppCompatActivity {
 
         binding = ActivityFeedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        /*View home = findViewById(R.id.navigation_home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeedActivity.this,  FeedActivity.class));
+            }
+        });*/
+
+        TextView user1 = findViewById(R.id.user1);
+        user1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeedActivity.this,  UserProfileActivity.class));
+            }
+        });
+
+        TextView user2 = findViewById(R.id.user2);
+        user2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeedActivity.this,  UserProfileActivity.class));
+            }
+        });
+
+        TextView user3 = findViewById(R.id.user3);
+        user3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeedActivity.this,  UserProfileActivity.class));
+            }
+        });
 
 
     }
--- repSocialApp/app/src/main/java/com/example/recipessocialapp/profilePages/UserProfileActivity.java
+++ repSocialApp/app/src/main/java/com/example/recipessocialapp/profilePages/UserProfileActivity.java
@@ -2,16 +2,18 @@ package com.example.recipessocialapp.profilePages;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.recipessocialapp.databinding.ActivityFeedBinding;
 
-public class userProfileActivity extends AppCompatActivity {
-    private ActivityFeedBinding binding;
+import com.example.recipessocialapp.databinding.ActivityUserprofile1Binding;
+
+
+public class UserProfileActivity extends AppCompatActivity {
+    private ActivityUserprofile1Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityFeedBinding.inflate(getLayoutInflater());
+        binding = ActivityUserprofile1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
