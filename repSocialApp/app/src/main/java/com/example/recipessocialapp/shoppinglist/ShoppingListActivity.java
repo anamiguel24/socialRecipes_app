20c28b8d63eb75e7410abdabcff20ac89af50944
florentino <florentino@ua.pt>
2022-05-29 22:04:37 +0100

    Shopping list, add button working

--- repSocialApp/app/src/main/java/com/example/recipessocialapp/shoppinglist/ShoppingListActivity.java
+++ repSocialApp/app/src/main/java/com/example/recipessocialapp/shoppinglist/ShoppingListActivity.java
@@ -1,10 +1,16 @@
 package com.example.recipessocialapp.shoppinglist;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,6 +43,41 @@ public class ShoppingListActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        LinearLayout custom = (LinearLayout) findViewById(R.id.custom);
+
+        ImageView plusB = findViewById(R.id.plusB);
+        plusB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                LinearLayout ly = new LinearLayout(getApplicationContext());
+                ly.setOrientation(LinearLayout.HORIZONTAL);
+                ly.setWeightSum(100);
+
+                CheckBox ingredient = new CheckBox(getApplicationContext());
+                ingredient.setWidth(500);
+                ingredient.setTextSize(16);
+                ingredient.setHeight(140);
+                ingredient.setButtonTintList(ColorStateList.valueOf(getResources().getColor(R.color.teal_200)));
+                EditText ing = findViewById(R.id.TypedIngredient);
+                ingredient.setText(ing.getText());
+
+                TextView quantity = new TextView(getApplicationContext());
+                EditText qty = findViewById(R.id.TypedQuantity);
+                quantity.setText(qty.getText());
+                quantity.setTextSize(16);
+                quantity.setHeight(140);
+                quantity.setTextColor(getResources().getColor(R.color.black));
+                quantity.setWidth(500);
+
+                ly.addView(ingredient);
+                ly.addView(quantity);
+
+                custom.addView(ly);
+            }
+        });
+
+
 
         BottomNavigationView bottom_bar = findViewById(R.id.bottom_bar);
         bottom_bar.setSelectedItemId(R.id.navigation_shoppinglist);
