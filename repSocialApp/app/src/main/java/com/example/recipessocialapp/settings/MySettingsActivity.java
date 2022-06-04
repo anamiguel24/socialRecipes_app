0ebcca44eddbd90da001e485fbbb2a0fb410eb9f
Ana Miguel <anamiguel24@ua.pt>
2022-06-04 13:22:40 +0100

    sign-out button programming

--- repSocialApp/app/src/main/java/com/example/recipessocialapp/settings/MySettingsActivity.java
+++ repSocialApp/app/src/main/java/com/example/recipessocialapp/settings/MySettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.recipessocialapp.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -8,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.recipessocialapp.R;
 import com.example.recipessocialapp.databinding.ActivityMysettingsBinding;
+import com.example.recipessocialapp.login.LoginActivity;
+import com.example.recipessocialapp.profilePages.MyProfileActivity;
 
 public class MySettingsActivity extends AppCompatActivity {
     private ActivityMysettingsBinding binding;
@@ -24,6 +27,14 @@ public class MySettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        View signoutB =findViewById(R.id.b_signOut);
+        signoutB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MySettingsActivity.this,  LoginActivity.class));
             }
         });
     }
